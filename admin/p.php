5013b6323a0f72b8f7146df8872853b8aa8e3a88
srinivas-0407 <117379200+srinivas-0407@users.noreply.github.com>
2023-06-13 07:37:34 +0530

    Add files via upload

---
+++ admin/p.php
@@ -0,0 +1,84 @@
+<html>
+    <head>
+    <meta charset="utf-8">
+        <link rel="stylesheet" href="css/signup.css">
+    </head>
+    <body>
+    <img style="height:80%;" src="loogo.png" alt="">
+  <div class="container">
+    <div class="title"><strong>Course Details</strong> </div>
+    <form  action="courses.php" method="POST" >
+      <?php
+         if(isset($_GET['error']))
+         {
+          echo $_GET['error'];
+         }
+         if(isset($_GET['sucess']))
+         {
+          ?>
+          <a href="a.html">Course was added go to home</a>
+          <?php
+         }
+       ?>
+      <div class="user-details">
+        <div class="input-box">
+          <span class="star" class="details">Course Name </span>
+            <input type="text" name='cname' placeholder="" required>
+        </div>
+        <div class="input-box">
+          <span class="star" class="details">Course Id </span>
+            <input type="text"  name='cid',placeholder="" required>
+        </div>
+        <div class="input-box">
+          <span  class="details">Level </span>
+          <input type="" name='level' list="Values">
+        <datalist id="Values">
+          <option value="advanced">
+            <option value="intermediate">
+              <option value="begineer">
+
+        </datalist>
+        </div>
+        <div class="input-box">
+          <span  class="details">Type</span>
+          <input type="" name='type' list="Value">
+        <datalist id="Value">
+          <option value="Paid">
+            <option value=Free>
+
+
+        </datalist>
+        </div>
+        <div class="input-box">
+          <span  class="details">Price</span>
+            <input type="text" name='price' placeholder="" >
+        </div>
+
+        <div  class="input-box">
+          <span class="star" class="details">Instructor Id</span>
+            <input type="text"  name='iid' placeholder="" required>
+        </div>
+
+                <div  class="input-box">
+                  <span  class="details">Category</span>
+                  <input name='category' type="" list="Valu">
+            <datalist id="Valu">
+              <option value="Computer Science">Computer Science</option>
+                <option value="Electrical">
+                  <option value="Data Science">
+                    <option value="Web Development">
+            </datalist>
+                </div>
+
+                        <div  class="input-box">
+                          <span class="details">Duration</span>
+                            <input type="text" name='duration' placeholder="" required>
+                        </div>
+      </div>
+      <div class="button">
+        <input type="submit" value="Add">
+      </div>
+    </form>
+  </div>
+  </body>
+</html>
